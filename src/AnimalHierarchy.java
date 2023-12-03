2fa2c4719365d15d3141c21ae93b396a31ea89d9
nourabyte <148945652+nourabyte@users.noreply.github.com>
2023-12-03 14:00:59 +0300

    Update AnimalHierarchy.java

--- src/AnimalHierarchy.java
+++ src/AnimalHierarchy.java
@@ -1,15 +1,13 @@
 public class AnimalHierarchy {
     public static void main(String[] args) {
-        Mammal lion = new Mammal("Simba", 5, "golden");
+        Mammal lion = new Mammal("Simba", 5);
         lion.eat();  
-        lion.sleep(); 
-        lion.giveBirth();  
-        System.out.println("Lion's fur color: " + lion.furColor);  
+        lion.sound(); 
+        System.out.println("Lion's sound is: " + lion.getsound);  
 
-        Bird eagle = new Bird("Sky", 3, 180);
+        Bird eagle = new Bird("Sky", 3);
         eagle.eat();  
-        eagle.sleep();
-        eagle.fly();  
-        System.out.println("Eagle's age: " + eagle.age);  
-        }
+        eagle.sound()
+        System.out.println("Eagle's age: " + eagle.getage);  
+    }
 }
